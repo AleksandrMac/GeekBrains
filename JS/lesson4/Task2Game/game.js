f0cf666b6b563a3789ee55f21ed1fbf921f96c67
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2020-11-06 11:00:14 +0900

    task2 completed

--- JS/lesson4/Task2Game/game.js
+++ JS/lesson4/Task2Game/game.js
@@ -4,29 +4,55 @@ const config = {
     startPositionX: 0,
     startPositionY: 0,
 };
-
 const player = {
     x: null,
     y: null,
+    step: [],
 
     init(startX, startY) {
         this.x = startX;
         this.y = startY;
     },
-
+    saveStep(direction) {this.step.push(direction)},
+    getStep(stepNumber) {                                                                           //task 2.1
+        if(stepNumber <= 0) alert('Нумерация шагов начинается с 1');
+        else if(this.step.length === 0) alert('Вы не сделали еще ни одного шага');
+        else if(stepNumber > this.step.length - 1) alert(`Вы совершили всего ${this.step.length-1} шагов.`);
+        else alert(this.step[stepNumber-1]);
+    },
+    isTop(y) {return y === 0},                                                                      //task 2.2
+    isDown(y) {return y === (config.rowCount - 1)},
+    isLeft(x) {return x === 0},
+    isRight(x) {return x === (config.colCount - 1)},
     move(direction) {
         switch (direction) {
             case 2:
-                this.y++;
+                if(!this.isDown(this.y)){
+                    this.y++;
+                    this.saveStep('down');
+                }
                 break;
             case 4:
-                this.x--;
+                if(!this.isLeft(this.x)){
+                    this.x--;
+                    this.saveStep('left');
+                }
+                break;
+            case 5:
+                const  stepNumber = parseInt(prompt('Введите номер шага:'))
+                this.getStep(stepNumber);
                 break;
             case 6:
-                this.x++;
+                if(!this.isRight(this.x)){
+                    this.x++;
+                    this.saveStep('right');
+                }
                 break;
             case 8:
-                this.y--;
+                if(!this.isTop(this.y)) {
+                    this.y--;
+                    this.saveStep('up');
+                }
                 break;
         }
     },
@@ -69,10 +95,10 @@ const game = {
     },
 
     getDirection() {
-        const availableDirections = [-1, 2, 4, 6, 8];
+        const availableDirections = [-1, 2, 4, 5, 6, 8];
 
         while (true) {
-            const direction = parseInt(prompt('Введите число куда хотите переместиться, -1 для выхода'));
+            const direction = parseInt(prompt('Введите число куда хотите переместиться, 5 получит ход по номеру, -1 для выхода'));
 
             if (!availableDirections.includes(direction)) {
                 alert(`Для перемещения необходимо ввести одно из чисел: ${availableDirections.join(', ')}.`);
