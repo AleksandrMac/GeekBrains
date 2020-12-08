4e9632fe0fb13d5ab061f3dfeed3956f6f3686ee
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2020-12-08 10:05:48 +0900

    sort modified

--- Go/Go_level_1/lesson3/sort/sort.go
+++ Go/Go_level_1/lesson3/sort/sort.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
-	slice := []int32{5, 5, 3, 9, 8, 6, 2, 1, 0, 6, 2}
+	slice := []int32{5, -2, 5, 3, 9, 8, 6, 2, 1, 0, 6, 2, -1}
+
 	sortSlice := bubbleSort(slice)
 	fmt.Println("unsort", slice)
 	fmt.Println("bubble sort", sortSlice)
+
 	sortSlice = insertSort(slice)
 	fmt.Println("unsort", slice)
 	fmt.Println("insert sort", sortSlice)
@@ -16,27 +18,29 @@ func main() {
 }
 
 func bubbleSort(slice []int32) []int32 {
-	sort := make([]int32, len(slice))
-	copy(sort, slice)
-	for i := 0; i < len(sort); i++ {
-		for i := range sort[:cap(sort)-1] {
-			if sort[i] > sort[i+1] {
-				sort[i], sort[i+1] = sort[i+1], sort[i]
+	sortedSlice := make([]int32, len(slice))
+	copy(sortedSlice, slice)
+	for i := 0; i < len(sortedSlice); i++ {
+		for i := range sortedSlice[:len(sortedSlice)-1] {
+			if sortedSlice[i] > sortedSlice[i+1] {
+				sortedSlice[i], sortedSlice[i+1] = sortedSlice[i+1], sortedSlice[i]
 			}
 		}
 	}
-	return sort
+	return sortedSlice
 
 }
 
 func insertSort(slice []int32) []int32 {
-	sort := make([]int32, 1, len(slice))
+	sortedSlice := make([]int32, len(slice))
 
 	for i := 0; i < len(slice); i++ {
-		sort = append(sort, slice[i])
-		for j := i; sort[j] > slice[i]; j-- {
-			sort[j], sort[j+1] = sort[j+1], sort[j]
+		key := slice[i]
+		j := i
+		for ; j > 0 && sortedSlice[j-1] > key; j-- {
+			sortedSlice[j] = sortedSlice[j-1]
 		}
+		sortedSlice[j] = key
 	}
-	return sort[1:]
+	return sortedSlice
 }
