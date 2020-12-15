25e6758f78cd320762556e150ebfb584b891fe08
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2020-12-15 15:18:56 +0900

    fibo updated

--- Go/Go_level_1/lesson4/fibo.go
+++ Go/Go_level_1/lesson4/fibo.go
@@ -2,24 +2,58 @@ package main
 
 import "fmt"
 
+// структура хранящая в себе числа фибоначи
+type fibo struct {
+	mapa map[int64]int64
+}
+
 func main() {
-	fiboMap := make(map[int64]int64)
+	fi := fibo{
+		mapa: map[int64]int64{},
+	}
+
 	var n int64
 	fmt.Scanln(&n)
 
 	for i := int64(0); i <= int64(n); i++ {
-		fiboMap[i] = fibo(int64(i))
+		fmt.Println(i, " - ", fi.Get(i))
 	}
-	fmt.Println(fiboMap)
 }
 
-func fibo(n int64) int64 {
+//получаем значение числа фибоначи
+func (f fibo) Get(n int64) int64 {
+	number, ok := f.mapa[n]
+
+	if ok {
+		return number
+	}
+	if n == 0 {
+		f.mapa[0] = 0
+		return 0
+	}
+	if n == 1 {
+		f.mapa[1] = 1
+		return 1
+	}
+	if f.mapa[n-1] > 0 {
+		return f.mapa[n-1] + f.mapa[n-2]
+	}
+	// если в мапе нет требуемого фибо,
+	// происходит до заполнение мапы до требуемого значения
+	f.Set(n)
+	//после заполнения получаем требуемое число
+	return f.Get(n)
+}
+func (f fibo) Set(n int64) {
 	switch n {
 	case 0:
-		return 0
+		f.mapa[0] = 0
 	case 1:
-		return 1
+		f.mapa[1] = 1
 	default:
-		return fibo(n-1) + fibo(n-int64(2))
+		if f.mapa[n-1] > 0 {
+			f.mapa[n] = f.mapa[n-1] + f.mapa[n-2]
+		}
+		f.Set(n - 1)
 	}
 }
