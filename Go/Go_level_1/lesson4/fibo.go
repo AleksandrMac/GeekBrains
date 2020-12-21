eda2c2bd6079820fbba86c0da02f02f5043649b6
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2020-12-21 13:56:25 +0900

    upgrade Fibo

--- Go/Go_level_1/lesson4/fibo.go
+++ Go/Go_level_1/lesson4/fibo.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// структура хранящая в себе числа фибоначи
+// fibo структура хранящая в себе числа фибоначи
 type fibo struct {
 	mapa map[int64]int64
 }
@@ -15,46 +15,33 @@ func main() {
 	var n int64
 	fmt.Scanln(&n)
 
-	for i := int64(0); i <= int64(n); i++ {
-		fmt.Println(i, " - ", fi.Get(i))
+	for i := int64(0); i <= n; i++ {
+		fmt.Println(i, " - ", fi.Fibo(i))
 	}
 }
 
-//получаем значение числа фибоначи
-func (f fibo) Get(n int64) int64 {
+// Fibo получаем значение числа фибоначи
+func (f fibo) Fibo(n int64) int64 {
 	number, ok := f.mapa[n]
 
 	if ok {
 		return number
 	}
-	if n == 0 {
-		f.mapa[0] = 0
-		return 0
-	}
-	if n == 1 {
-		f.mapa[1] = 1
-		return 1
-	}
-	if f.mapa[n-1] > 0 {
-		return f.mapa[n-1] + f.mapa[n-2]
-	}
-	// если в мапе нет требуемого фибо,
-	// происходит до заполнение мапы до требуемого значения
-	f.Set(n)
-	//после заполнения получаем требуемое число
-	return f.Get(n)
-}
-func (f fibo) Set(n int64) {
 	switch n {
 	case 0:
 		f.mapa[0] = 0
+		return 0
 	case 1:
+		f.mapa[0] = 0
 		f.mapa[1] = 1
+		return 1
 	default:
+		// если в мапе нет требуемого фибо,
+		// происходит до заполнение мапы до требуемого значения
 		if f.mapa[n-1] > 0 {
 			f.mapa[n] = f.mapa[n-1] + f.mapa[n-2]
-		} else {
-			f.Set(n - 1)
+			return f.mapa[n]
 		}
+		return f.Fibo(n-1) + f.Fibo(n-2)
 	}
 }
