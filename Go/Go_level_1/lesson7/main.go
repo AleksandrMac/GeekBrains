34583b12961e80e2c0d4023ad331b0cee69ac0ca
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-01-06 18:40:16 +0900

    add config (yaml and json), validation

--- Go/Go_level_1/lesson7/main.go
+++ Go/Go_level_1/lesson7/main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"lesson7/config"
+	"lesson7/valid"
 	"log"
-	"os"
 )
 
 func main() {
-	conf := make(map[string]string)
+	conf := make(map[string]interface{})
 	err := make(map[string]error)
-	var file *os.File
-	println(os.Getwd())
-	file, err["file"] = os.Open("conf.yaml")
-	fmt.Println(file.Name())
-	if err["file"] != nil {
-		log.Fatal(err)
+
+	//conf, err = config.Config("conf.json", "json", "db_url")
+	conf, err = config.Config("conf.yaml", "yaml", "db_url")
+	//Валидация
+	_, ok := conf["port"]
+	if ok && !valid.IsPort(conf["port"].(float64)) {
+		err["port"] = errors.New("Номер порта указан не корректно")
 	}
-	delete(err, "file")
 
-	defer func() {
-		file.Close()
-		if len(err) > 0 {
-			log.Fatal(err)
-		}
-	}()
+	_, ok = conf["db_url"]
+	if ok {
+		_, err["db_url"] = valid.IsURL(conf["db_url"].(string))
+	}
 
-	conf, err = config.Config(file)
+	_, ok = conf["jaeger_url"]
+	if ok {
+		_, err["jaeger_url"] = valid.IsURL(conf["jaeger_url"].(string))
+	}
+
+	_, ok = conf["sentry_url"]
+	if ok {
+		_, err["sentry_url"] = valid.IsURL(conf["sentry_url"].(string))
+	}
 
 	fmt.Println(conf)
 	if len(err) > 0 {
--- Go/Go_level_1/lesson7/valid/valid.go
+++ Go/Go_level_1/lesson7/valid/valid.go
@@ -1,66 +1,20 @@
 package valid
 
-import "strconv"
+import (
+	"math"
+	"net/url"
+)
 
-func isPort(port string) bool {
-	n, _ := strconv.Atoi(port)
-	return n < 65536
+//IsPort -
+func IsPort(port float64) bool {
+	return uint64(port) < math.MaxUint16
 }
 
-func isProtocol(protocol string) bool {
-	switch protocol {
-	case "git", "postgres", "http", "https", "ftp":
-		return true
-	default:
-		return false
+//IsURL  -
+func IsURL(urlStr string) (bool, error) {
+	_, err := url.Parse(urlStr)
+	if err == nil {
+		return true, err
 	}
+	return false, err
 }
-
-func isURL(port string) bool {
-	return false
-}
-
-/*ftp — протокол передачи файлов FTP
-http — протокол передачи гипертекста HTTP
-rtmp — проприетарный протокол потоковой передачи данных Real Time Messaging Protocol, в основном используется для передачи потокового видео и аудио с веб-камер через Интернет.
-rtsp — потоковый протокол реального времени.
-https — специальная реализация протокола HTTP, использующая шифрование (как правило, SSL или TLS)
-gopher — протокол Gopher
-mailto — адрес электронной почты
-news — новости Usenet
-nntp — новости Usenet через протокол NNTP
-irc — протокол IRC
-smb — протокол SMB/CIFS
-prospero — служба каталогов Prospero Directory Service
-telnet — ссылка на интерактивную сессию Telnet
-wais — база данных системы WAIS
-xmpp — протокол XMPP (часть Jabber)
-file — имя локального файла
-data — непосредственные данные (Data: URL)
-tel — звонок по указанному телефону
-Экзотические схемы URL:
-
-afs — глобальное имя файла в файловой системе Andrew File System
-cid — идентификатор содержимого для частей MIME
-mid — идентификатор сообщений для электронной почты
-mailserver — доступ к данным с почтовых серверов
-nfs — имя файла в сетевой файловой системе NFS
-tn3270 — эмуляция интерактивной сессии Telnet 3270
-z39.50 — доступ к службам ANSI Z39.50
-skype — протокол Skype
-smsto — открытие редактора SMS в некоторых мобильных телефонах
-ed2k — файлообменная сеть eDonkey, построенная по принципу P2P
-market — Android-маркет
-steam — протокол Steam
-bitcoin — криптовалюта Биткойн
-ob — OpenBazaar
-tg — Telegram
-Схемы URL в браузерах:
-
-view-source — просмотр исходного кода указанной веб-страницы в различных браузерах.
-В разных браузерах используются разные ключевые слова для доступа к служебным и сервисным страницам:
-chrome в браузерах Google Chrome или браузеров на движке Gecko[4].
-about в Firefox и других
-opera в Opera
-browser в Яндекс.Браузер.
-*/
