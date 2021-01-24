38efbd7a7d11dbee0720ae271ed4d2311d6d441a
AleksandrMac <k-t_monkey@mail.ru>
2021-01-24 17:29:43 +0900

    FindBin completed

--- Algoritm/lesson3/sort.go
+++ Algoritm/lesson3/sort.go
@@ -27,9 +27,33 @@ func Main() {
 	fmt.Println("unsort", slice)
 	fmt.Println("insert sort", sortSlice)
 
+	sortSlice = []int32{}
+	for i := int32(1); i <= 100; i++ {
+		sortSlice = append(sortSlice, i)
+	}
+	fmt.Println("slice", sortSlice)
+	fmt.Println("find position", FindBin(0, sortSlice))
+	fmt.Println("find position", FindBin(1, sortSlice))
+	fmt.Println("find position", FindBin(2, sortSlice))
+	fmt.Println("find position", FindBin(3, sortSlice))
+	fmt.Println("find position", FindBin(4, sortSlice))
+	fmt.Println("find position", FindBin(5, sortSlice))
+	fmt.Println("find position", FindBin(6, sortSlice))
+	fmt.Println("find position", FindBin(7, sortSlice))
+	fmt.Println("find position", FindBin(8, sortSlice))
+	fmt.Println("find position", FindBin(9, sortSlice))
+	fmt.Println("find position", FindBin(10, sortSlice))
+	fmt.Println("find position", FindBin(11, sortSlice))
+	fmt.Println("find position", FindBin(12, sortSlice))
 
-	fmt.Println("insert sort", FindBin())
-
+	fmt.Println("find position", FindBin(49, sortSlice))
+	fmt.Println("find position", FindBin(50, sortSlice))
+	fmt.Println("find position", FindBin(51, sortSlice))
+	fmt.Println("find position", FindBin(97, sortSlice))
+	fmt.Println("find position", FindBin(98, sortSlice))
+	fmt.Println("find position", FindBin(99, sortSlice))
+	fmt.Println("find position", FindBin(100, sortSlice))
+	fmt.Println("find position", FindBin(101, sortSlice))
 }
 
 //BubbleSort -
@@ -123,7 +147,35 @@ func ShakerSort(slice []int32) []int32 {
 	return sortedSlice
 }
 
+//3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив.
+//Функция возвращает индекс найденного элемента или -1, если элемент не найден.
+
 //FindBin -
-func p []int32 FindBin(slice []int32) int32 {
-	return 0
+func FindBin(value int32, slice []int32) int32 {
+	l := int32(len(slice))
+	left := l / 2
+	right := l - left
+	mid := left
+	for i := int32(1); mid >= 0 || mid <= int32(len(slice)-1); i++ {
+		if value == slice[mid] {
+			return mid
+		}
+		if mid == 0 || mid == int32(len(slice)-1) {
+			return -1
+		}
+
+		if value < slice[mid] {
+			l = left
+			left = l / 2
+			right = l - left
+			mid = mid - right
+			continue
+		}
+		l = right
+		left = l / 2
+		right = l - left
+		mid = mid + left
+	}
+
+	return -1
 }
