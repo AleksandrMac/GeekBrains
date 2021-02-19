fab6830a22b87b88dbd93ff8aa7436489dc03520
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-02-19 18:57:50 +0900

    task2 completed

--- Go/Go_level_2/lesson1/error.go
+++ Go/Go_level_2/lesson1/error.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+//ErrorWithTime -
+type ErrorWithTime struct {
+	text string
+	time string
+}
+
+//New -
+func New(text string) error {
+	return &ErrorWithTime{
+		text: text,
+		time: time.Now().Format(time.RFC1123),
+	}
+}
+func (e *ErrorWithTime) Error() string {
+	return fmt.Sprintf("error:\t%s\t%s:", e.text, e.time)
+}
 func main() {
 	defer func() {
 		if v := recover(); v != nil {
-			fmt.Println("recovered", v)
+			var err error
+			err = New("recovered")
+			fmt.Printf("%v\n%v", err, v)
 		}
 	}()
 
