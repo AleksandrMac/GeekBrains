7788da82347caa8c759d8669a7ddb93d11a0e0c9
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-03-05 14:25:52 +0900

    task1 completed

---
+++ Go/Go_level_2/lesson5/task/task1.go
@@ -0,0 +1,23 @@
+package task
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Task1
+// Напишите программу, которая запускает n потоков и дожидается завершения их всех
+func Task1() {
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("end")
+}
