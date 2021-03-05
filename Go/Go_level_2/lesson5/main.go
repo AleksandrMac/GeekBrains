32fd3835e526e740a2d6ee802bed8a5117a86602
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-03-05 14:54:33 +0900

    task 2 completed

--- Go/Go_level_2/lesson5/main.go
+++ Go/Go_level_2/lesson5/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/AleksandrMac/GeekBrains/Go/Go_level_2/lesson5/task"
+import (
+	"fmt"
+
+	"github.com/AleksandrMac/GeekBrains/Go/Go_level_2/lesson5/task"
+)
 
 func main() {
+	fmt.Println("----------Task1-------------")
 	task.Task1()
+	fmt.Println("----------Task1-------------")
+	task.Task2()
 }
