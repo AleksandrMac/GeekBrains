3ea7565e75a49ca83910d0b034a87ebc989735a2
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-03-30 17:19:41 +0900

    task 2 for circuit breaker(half-open) completed

--- Go/go_best_practice/lesson1/practice/circuit_breaker/client/client.go
+++ Go/go_best_practice/lesson1/practice/circuit_breaker/client/client.go
@@ -13,16 +13,52 @@ import (
 func main() {
 	address := "127.0.0.1:5433"
 	timeout := 1 * time.Second
+	//кол-во недачных соединений после которых закрывам канал
+	maxBadConnect := 10
+	//процент успешных соединений
+	succesConnectProcent := 100
+	halfOpen := false
+
 	for {
 		// паттерн circuit breaker в простейшей реализации
 		// в реальности за функцией writingToServer может скрываться объёмная и сложная логика
 		// лишнее выполнение которой мы и хотим прервать
 		if isServerAlive(address, timeout) {
-			// если сервер стал доступен, возвращаем логику работы с ним
-			connectAndWrite(address, timeout)
+			maxBadConnect = 10
+			log.Println(halfOpen, "   ", maxBadConnect, "     ", succesConnectProcent)
+			if halfOpen {
+
+				//для оценки процента успешных соединений рассматриваем последнии maxBadConnect * 2 соединений
+				//работаем с целыми числами т.к. этой точности достасточно
+				succesConnectProcent += 100 / (maxBadConnect * 2)
+				//паузами регулируем плавный возврат к рабочему режиму
+				log.Println(succesConnectProcent)
+				if succesConnectProcent < 70 {
+					time.Sleep(4 * time.Second)
+				} else if succesConnectProcent < 90 {
+					time.Sleep(3 * time.Second)
+				} else {
+					//после выхода в рабочей режим снимаем флаг полуоткрытости
+					halfOpen = false
+					succesConnectProcent = 100
+				}
+				connectAndWrite(address, timeout)
+			} else {
+				// если сервер стал доступен, возвращаем логику работы с ним
+				connectAndWrite(address, timeout)
+			}
+		} else {
+			log.Println(halfOpen, "   ", maxBadConnect)
+			if !halfOpen {
+				maxBadConnect--
+				if maxBadConnect == 0 {
+					halfOpen = true
+					succesConnectProcent = 50
+				}
+			}
 		}
 		// частота опроса проверки жив ли сервер
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -62,5 +98,5 @@ func connectAndWrite(address string, timeout time.Duration) error {
 		}
 		log.Printf("Message from server: " + message)
 	}
-	return nil
+	//return nil
 }
