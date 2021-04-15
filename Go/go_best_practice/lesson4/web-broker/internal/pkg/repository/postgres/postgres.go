930e3476816b6f90c29daef41c28a6b5400853f8
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-04-15 15:20:04 +0900

    task1 completed

---
+++ Go/go_best_practice/lesson4/web-broker/internal/pkg/repository/postgres/postgres.go
@@ -0,0 +1,19 @@
+package postgres
+
+import "github.com/vlslav/web-broker/internal/pkg/model"
+
+type PgRepo struct{}
+
+func New() *PgRepo {
+	return &PgRepo{}
+}
+
+func (pgr *PgRepo) Get(getReq string) (string, error) {
+	// TODO: impl
+	return "", nil
+}
+
+func (pgr *PgRepo) Put(putReq *model.PutValue) error {
+	// TODO: impl
+	return nil
+}
