6911f560c8ae1fac9ab2a2257baf29896dbe9ec1
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-04-22 09:09:26 +0900

    TestInfixToPostfix completed

--- Go/go_best_practice/csv_query/csv/read_test.go
+++ Go/go_best_practice/csv_query/csv/read_test.go
@@ -17,7 +17,7 @@ type getItemData struct {
 	Operation, Values []string
 }
 
-func TestSplit(t *testing.T) {
+func TestSplitReverse(t *testing.T) {
 	left, right := where, ""
 	want := []splitData{
 		{Left: "(continent='Asia' AND (date>'2020-04-14' AND date < '2020-04-20') OR (continent='Africa' AND '2020-04-14' != date)", Right: ")"},
@@ -47,7 +47,7 @@ func TestSplit(t *testing.T) {
 		{Left: "", Right: "("},
 	}
 	for _, val := range want {
-		left, right = csv.Split(left)
+		left, right = csv.SplitReverse(left)
 		got := splitData{Left: left, Right: right}
 		assert.Equal(t, val, got, "they should be equal")
 	}
@@ -56,9 +56,20 @@ func TestSplit(t *testing.T) {
 func TestGetItem(t *testing.T) {
 	want := getItemData{
 		Operation: []string{")", ")", "!=", "AND", "=", "(", "OR", ")", "<", "AND", ">", "(", "AND", "=", "("},
-		Values:    []string{"date", "'2020-04-14'", "'Africa'", "continent", "'2020-04-20'", "date", "'2020-04-14'", "date", "'Asia'", "continent"},
+		Values:    []string{"'2020-04-14'", "'Africa'", "continent", "'2020-04-20'", "date", "'2020-04-14'", "date", "'Asia'", "continent"},
 	}
 	got := getItemData{}
 	got.Operation, got.Values = csv.GetItem(where)
+	assert.Equal(t, want, got, "func \"TestGetItem\": they should be equal")
+}
+
+func TestGetLex(t *testing.T) {
+	want := []string{"(", "continent", "=", "'Asia'", "AND", "(", "date", ">", "'2020-04-14'", "AND", "date", "<", "'2020-04-20'", ")", "OR", "(", "continent", "=", "'Africa'", "AND", "'2020-04-14'", "!=", "date", ")", ")"}
+	got := csv.GetLex(where)
+	assert.Equal(t, want, got, "they should be equal")
+}
+func TestInfixToPostfix(t *testing.T) {
+	want := []string{"continent", "'Asia'", "=", "date", "'2020-04-14'", ">", "date", "'2020-04-20'", "<", "AND", "continent", "'Africa'", "=", "'2020-04-14'", "date", "!=", "AND", "OR", "AND"}
+	got := csv.InfixToPostfix(csv.GetLex(where))
 	assert.Equal(t, want, got, "func \"TestGetItem\": they should be equal")
 }
