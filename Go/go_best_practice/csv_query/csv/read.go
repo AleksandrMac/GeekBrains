9560cb9aa5a935ace0667634cacdb38d2415af75
AleksandrMac <62098405+AleksandrMac@users.noreply.github.com>
2021-04-22 09:08:58 +0900

    InfixToPostfix completed

--- Go/go_best_practice/csv_query/csv/read.go
+++ Go/go_best_practice/csv_query/csv/read.go
@@ -40,8 +40,66 @@ func (d *Row) IsMatch(match string) bool {
 	match = strings.TrimSpace(match)
 	match = strings.ToUpper(match)
 
-	op, val := GetItem(match)
+	lexInfix := GetLex(match)
+	InfixToPostfix(lexInfix)
+
+	// for lop, lval := len(op), len(val); lop > 0 && lval > 0; {
+	// 	// o := op[lop-1]
+	// 	// leftExp, rightExp := "", ""
+
+	// }
 	return true
+}
+
+func InfixToPostfix(infix []string) (postfix []string) {
+	stack := make([]string, 0, len(infix))
+	for _, val := range infix {
+		switch val {
+		case "(":
+			stack = append(stack, val)
+		case ")":
+			i := len(stack) - 1
+			for ; stack[i] != "("; i-- {
+				postfix = append(postfix, stack[i])
+			}
+			stack = stack[:i]
+		case "+", "-", "!", "NOT", "/", "*", "DIV", "MOD", "AND", "OR", "<", ">", "<=", ">=", "<>", "!=", "=":
+			//i := len(stack) - 1
+			for i := len(stack) - 1; GetPriority(stack[i]) >= GetPriority(val) && i >= 0; i = len(stack) - 1 {
+				postfix = append(postfix, stack[i])
+				stack = stack[:i]
+			}
+			stack = append(stack, val)
+		default:
+			postfix = append(postfix, val)
+		}
+	}
+	return postfix
+}
+
+func GetPriority(operator string) uint8 {
+	operator = strings.ToUpper(operator)
+	switch operator {
+	case "(", ")":
+		return 1
+	case "+", "-", "!", "NOT":
+		return 2
+	case "/", "*", "DIV", "MOD", "AND":
+		return 3
+	case "OR":
+		return 4
+	case "<", ">", "<=", ">=", "<>", "!=", "=":
+		return 5
+	default:
+		return 0
+	}
+}
+func GetLex(str string) (lex []string) {
+	for left, right := "", str; len(right) > 0; {
+		left, right = Split(right)
+		lex = append(lex, left)
+	}
+	return lex
 }
 func GetItem(str string) (op, val []string) {
 	for left, right := str, ""; len(left) > 0; {
@@ -59,7 +117,57 @@ func GetItem(str string) (op, val []string) {
 	}
 	return op, val
 }
+
 func Split(str string) (left, right string) {
+	inputBuffer := []byte(str)
+	outputBuffer := make([]byte, 0, len(inputBuffer))
+	spaceIgnoring := false
+	for i, val := range inputBuffer {
+		switch val {
+		case ')', '(':
+			if len(outputBuffer) > 0 {
+				return string(outputBuffer), string(inputBuffer[i:])
+			}
+			return string(val), string(inputBuffer[i+1:])
+		case '>', '<', '!', '=':
+			if len(outputBuffer) > 0 {
+				return string(outputBuffer), string(inputBuffer[i:])
+			}
+			outputBuffer = append(outputBuffer, val)
+			switch inputBuffer[i+1] {
+			case '=':
+				outputBuffer = append(outputBuffer, inputBuffer[i+1])
+				return string(outputBuffer), string(inputBuffer[i+2:])
+			default:
+				return string(outputBuffer), string(inputBuffer[i+1:])
+			}
+		default:
+			switch val {
+			case ' ':
+				if !spaceIgnoring {
+					if len(outputBuffer) > 0 {
+						return string(outputBuffer), string(inputBuffer[i+1:])
+					}
+					continue
+				}
+				outputBuffer = append(outputBuffer, ' ')
+			case []byte("'")[0]:
+				if !spaceIgnoring {
+					spaceIgnoring = true
+					outputBuffer = append(outputBuffer, val)
+					continue
+				}
+				outputBuffer = append(outputBuffer, val)
+				return string(outputBuffer), string(inputBuffer[i+1:])
+			default:
+				outputBuffer = append(outputBuffer, val)
+			}
+		}
+	}
+	return string(outputBuffer), string(inputBuffer)
+}
+
+func SplitReverse(str string) (left, right string) {
 	inputBuffer := []byte(str)
 	outputBuffer := make([]byte, 0, len(inputBuffer))
 	spaceIgnoring := false
